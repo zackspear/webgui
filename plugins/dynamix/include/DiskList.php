d3c7dde801b77494ad0fda230dc7182ae55d5e34
bergware <ronald.vd.berg62@gmail.com>
2017-09-02 09:33:21 +0200

    Change icon in share-list and disk-list which is partially encrypted

--- plugins/dynamix/include/DiskList.php
+++ plugins/dynamix/include/DiskList.php
@@ -74,7 +74,7 @@ foreach ($disks as $name => $disk) {
   switch ($disk['luksState']) {
     case 0: $luks = ""; break;
     case 1: $luks = "<i class='padlock green-text fa fa-lock' title='All files in share encrypted'></i>"; break;
-    case 2: $luks = "<i class='padlock orange-text fa fa-unlock-alt' title='Some files in share unencrypted'></i>"; break;
+    case 2: $luks = "<i class='padlock orange-text fa fa-lock' title='Some files in share unencrypted'></i>"; break;
    default: $luks = ""; break;
   }
   echo "<tr>";
