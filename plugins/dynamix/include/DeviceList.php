cc6f8ebe2069366a372b74b5165b83cce729b083
bergware <ronald.vd.berg62@gmail.com>
2018-01-28 08:20:12 +0100

    Round size in parity operation progress indicators

--- plugins/dynamix/include/DeviceList.php
+++ plugins/dynamix/include/DeviceList.php
@@ -402,7 +402,7 @@ case 'open':
 case 'parity':
   $data = [];
   if ($var['mdResync']>0) {
-    $data[] = my_scale($var['mdResync']*1024,$unit)." $unit";
+    $data[] = my_scale($var['mdResync']*1024,$unit,-1)." $unit";
     $data[] = my_clock(floor((time()-$var['sbUpdated'])/60));
     $data[] = my_scale($var['mdResyncPos']*1024,$unit)." $unit (".number_format(($var['mdResyncPos']/($var['mdResync']/100+1)),1,substr($display['number'],0,1),'')." %)";
     $data[] = my_scale($var['mdResyncDb']*1024/$var['mdResyncDt'],$unit, 1)." $unit/sec";
