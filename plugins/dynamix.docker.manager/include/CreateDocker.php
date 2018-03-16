99b76766a046b558e64222a3ca5b2d54371bcb4d
bergware <ronald.vd.berg62@gmail.com>
2018-03-16 20:07:09 +0100

    Inhibit arbitrary command execution in Docker post arguments

--- plugins/dynamix.docker.manager/include/CreateDocker.php
+++ plugins/dynamix.docker.manager/include/CreateDocker.php
@@ -433,7 +433,7 @@ function xmlToCommand($xml, $create_paths=false) {
                  implode(' --device=', $Devices),
                  $xml['ExtraParams'],
                  escapeshellarg($xml['Repository']),
-                 $xml['PostArgs']);
+                 escapeshellarg($xml['PostArgs']));
   return [preg_replace('/\s+/', ' ', $cmd), $xml['Name'], $xml['Repository']];
 }
 
