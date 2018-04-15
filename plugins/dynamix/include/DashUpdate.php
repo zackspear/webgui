f4f78eea4783ad896a3bd7284b1f02d6bbdb80d2
bergware <ronald.vd.berg62@gmail.com>
2018-04-15 19:03:15 +0200

    Regression CSS fixes for OS upgrade page

--- plugins/dynamix/include/DashUpdate.php
+++ plugins/dynamix/include/DashUpdate.php
@@ -379,7 +379,7 @@ case 'parity':
     echo my_clock(floor($t/60));
     echo "</em>";
   } else {
-    echo "<i class='fa fa-warning'></i> Please start a Parity-sync immediately";
+    echo "<i class='fa fa-warning'></i> Please start a Parity-Sync immediately";
   }
 break;
 case 'shares':
