5a728b1d7260e0aa1eebd7866a9338ddd2bc1441
bergware <ronald.vd.berg62@gmail.com>
2018-08-01 09:37:58 +0200

    Fixed 'Update All Containers' button sometimes hidden in docker list

--- plugins/dynamix.docker.manager/javascript/docker.js
+++ plugins/dynamix.docker.manager/javascript/docker.js
@@ -186,7 +186,7 @@ function resumeAll() {
 function checkAll() {
   $('input[type=button]').prop('disabled',true);
   $('.updatecolumn').html('<span style="color:#267CA8"><i class="fa fa-refresh fa-spin"></i> checking...</span>');
-  $.post('/plugins/dynamix.docker.manager/include/DockerUpdate.php',{check:true},function(u){loadlist(u);});
+  loadlist();
 }
 function updateAll() {
   $('input[type=button]').prop('disabled',true);
