a970b201a53bdf2bf6344694a0f3150ea5a5478f
bergware <ronald.vd.berg62@gmail.com>
2018-08-22 15:08:48 +0200

    Replaced blue circle animation with unraid wave

--- plugins/dynamix.docker.manager/javascript/docker.js
+++ plugins/dynamix.docker.manager/javascript/docker.js
@@ -190,7 +190,7 @@ function checkAll() {
 }
 function updateAll() {
   $('input[type=button]').prop('disabled',true);
-  $('div.spinner').show('slow');
+  $('div.spinner').addClass('fixed').show('slow');
   var list = '';
   for (var i=0,ct; ct=docker[i]; i++) if (ct.update=='false') list += '&ct[]='+encodeURI(ct.name);
   var address = '/plugins/dynamix.docker.manager/include/CreateDocker.php?updateContainer=true'+list;
--- plugins/dynamix/include/DefaultPageLayout.php
+++ plugins/dynamix/include/DefaultPageLayout.php
@@ -483,6 +483,9 @@ $('.back_to_top').click(function(event) {
   return false;
 });
 $(function() {
+  // unraid animated logo
+  var unraid_logo = '<svg viewBox="0 0 133.52 76.97" class="unraid_mark"><defs><linearGradient id="gradient" class="gradient" x1="23.76" y1="81.49" x2="109.76" y2="-4.51" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#e32929"/><stop offset="1" stop-color="#ff8d30"/></linearGradient></defs><path d="m70,19.24zm57,0l6.54,0l0,38.49l-6.54,0l0,-38.49z" fill="url(#gradient)" class="unraid_mark_9"/><path d="m70,19.24zm47.65,11.9l-6.55,0l0,-23.79l6.55,0l0,23.79z" fill="url(#gradient)" class="unraid_mark_8"/><path d="m70,19.24zm31.77,-4.54l-6.54,0l0,-14.7l6.54,0l0,14.7z" fill="url(#gradient)" class="unraid_mark_7"/><path d="m70,19.24zm15.9,11.9l-6.54,0l0,-23.79l6.54,0l0,23.79z" fill="url(#gradient)" class="unraid_mark_6"/><path d="m63.49,19.24l6.51,0l0,38.49l-6.51,0l0,-38.49z" fill="url(#gradient)" class="unraid_mark_5"/><path d="m70,19.24zm-22.38,26.6l6.54,0l0,23.78l-6.54,0l0,-23.78z" fill="url(#gradient)" class="unraid_mark_4"/><path d="m70,19.24zm-38.26,43.03l6.55,0l0,14.73l-6.55,0l0,-14.73z" fill="url(#gradient)" class="unraid_mark_3"/><path d="m70,19.24zm-54.13,26.6l6.54,0l0,23.78l-6.54,0l0,-23.78z" fill="url(#gradient)" class="unraid_mark_2"/><path d="m70,19.24zm-63.46,38.49l-6.54,0l0,-38.49l6.54,0l0,38.49z" fill="url(#gradient)" class="unraid_mark_1"/></svg>';
+
   if ($("div#nav-right").css('float').toLowerCase() == 'right') {
     var origNavRightWidth = $('div#nav-right').width();
     var adjustMenuSize = function() {
@@ -572,6 +575,7 @@ $(function() {
     });
   }
   $('form').append($('<input>').attr({type:'hidden', name:'csrf_token', value:'<?=$var['csrf_token']?>'}));
+  $('div.spinner').each(function(){$(this).html(unraid_logo);});
   watchdog.start();
 });
 </script>
