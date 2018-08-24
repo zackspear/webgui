ce990136f7c54bb775183513388a35c7708909e2
bergware <ronald.vd.berg62@gmail.com>
2018-08-24 11:28:24 +0200

    Header and style corrections

--- plugins/dynamix/include/DefaultPageLayout.php
+++ plugins/dynamix/include/DefaultPageLayout.php
@@ -86,13 +86,12 @@ function updateTime() {
   if (expiretime > 0) {
     var remainingtime = expiretime - now.getTime()/1000;
     if (remainingtime <= 0) {
-      $('#licenseexpire').html('<em> (click here for details)</em>');
-      $('#licensetype').addClass('warning')
+      $('#licenseexpire').html('Trial period: expired (click here for details)');
     } else {
       days = parseInt(remainingtime/86400);
       hour = parseInt(remainingtime/3600%24);
       mins = parseInt(remainingtime/60%60);
-      $('#licenseexpire').html(' - '+((days|hour|mins)?(days?plus(days,'day',true):(hour?plus(hour,'hour',true):plus(mins,'minute',true))):'less than a minute')+' remaining (click here for details)');
+      $('#licenseexpire').html('Trial period: '+((days|hour|mins)?(days?plus(days,'day',true):(hour?plus(hour,'hour',true):plus(mins,'minute',true))):'less than a minute')+' remaining (click here for details)');
     }
   }
   setTimeout(updateTime,1000);
@@ -296,13 +295,14 @@ $.ajaxPrefilter(function(s, orig, xhr){
   <div class="upgrade_notice" style="display:none"></div>
   <div id="header" class="<?=$display['banner']?>">
    <div class="logo">
-   <a href="#" onclick="openBox('/webGui/include/Feedback.php','Feedback',600,600,false);return false;"><img src="/webGui/images/UN-logotype-gradient.png" title="Feedback" border="0"/></a>
-   <a href="/Tools/Registration"><span></span></a>
+   <a href="#" onclick="openBox('/webGui/include/Feedback.php','Feedback',600,600,false);return false;"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 222.36 39.04"><defs><linearGradient id="header-logo" x1="47.53" y1="79.1" x2="170.71" y2="-44.08" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#e32929"/><stop offset="1" stop-color="#ff8d30"/></linearGradient></defs><title>Feedback</title><path d="M146.7,29.47H135l-3,9h-6.49L138.93,0h8l13.41,38.49h-7.09L142.62,6.93l-5.83,16.88h8ZM29.69,0V25.4c0,8.91-5.77,13.64-14.9,13.64S0,34.31,0,25.4V0H6.54V25.4c0,5.17,3.19,7.92,8.25,7.92s8.36-2.75,8.36-7.92V0ZM50.86,12v26.5H44.31V0h6.11l17,26.5V0H74V38.49H67.9ZM171.29,0h6.54V38.49h-6.54Zm51.07,24.69c0,9-5.88,13.8-15.17,13.8H192.67V0H207.3c9.18,0,15.06,4.78,15.06,13.8ZM215.82,13.8c0-5.28-3.3-8.14-8.52-8.14h-8.08V32.77h8c5.33,0,8.63-2.8,8.63-8.08ZM108.31,23.92c4.34-1.6,6.93-5.28,6.93-11.55C115.24,3.68,110.18,0,102.48,0H88.84V38.49h6.55V5.66h6.87c3.8,0,6.21,1.82,6.21,6.71s-2.41,6.76-6.21,6.76H98.88l9.21,19.36h7.53Z" fill="url(#header-logo)"/></svg></a>
+   <a href="/Tools/Registration"><span id="licenseexpire"></span></a>
    </div>
    <div class="block">
     <span class="text-left">Server<br/>Description<br/>Version<br/>Uptime</span>
-    <span class="text-right"><?=$var['NAME']." &bullet; ".$eth0['IPADDR:0']?><br/><?=$var['COMMENT']?><br/><?=$var['version']?>
-    <a href="/Tools/Registration" class="header" title="Go to Registration page"><span id="licensetype"><?=$var['regTy']?></span></a><?=$notes?><br/><span id="uptime"></span></span>
+    <span class="text-right"><?=$var['NAME']." &bullet; ".$eth0['IPADDR:0']?><br/><?=$var['COMMENT']?><br/>
+    <a href="/Tools/Registration" class="header" title="Go to Registration page"><?=$var['version']?>-<span id="licensetype"><?=$var['regTy']?></span></a><?=$notes?><br/>
+    <span id="uptime"></span></span>
    </div>
   </div>
   <a href="#" class="back_to_top" title="Back To Top"><i class="fa fa-arrow-circle-up"></i></a>
