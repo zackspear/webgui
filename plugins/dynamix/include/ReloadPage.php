204d9fb25c3c5eb5c37c7067df44f5c2d4ae5842
bergware <ronald.vd.berg62@gmail.com>
2018-12-25 10:28:59 +0100

    Copyright year update

--- plugins/dynamix/include/ReloadPage.php
+++ plugins/dynamix/include/ReloadPage.php
@@ -1,6 +1,6 @@
 <?PHP
 /* Copyright 2005-2018, Lime Technology
- * Copyright 2015-2017, Bergware International
+ * Copyright 2015-2018, Bergware International
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2,
