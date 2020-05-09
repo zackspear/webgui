0a979e0f6126a8737829c08dbb5a7040ac2882fe
bergware <bergware@users.noreply.github.com>
2020-05-09 20:37:18 +0200

    Multi-language support

--- plugins/dynamix/include/Translations.php
+++ plugins/dynamix/include/Translations.php
@@ -66,52 +66,48 @@ function translate($key) {
   return !$plug;
 }
 $language = [];
-$locale   = $_SESSION['locale'];
-$return   = 'function _(t){return t;}';
-$helpsrc  = "$docroot/webGui/help/helptext.txt";
-$helptxt  = "$docroot/webGui/help/helptext.dot";
+$locale   = $_SESSION['locale'] ?: 'en_US';
+$return   = "function _(t){return t;}";
+$text     = "$docroot/languages/$locale/translations.txt";
+$help     = "$docroot/languages/$locale/helptext.txt";
 
-if ($locale) {
-  $text = "$docroot/languages/$locale/translations.txt";
-  if (file_exists($text)) {
-    $basis = "$docroot/languages/$locale/translations.dot";
-    // global translations
-    if (!file_exists($basis)) file_put_contents($basis,serialize(parse_lang_file($text)));
-    $language = unserialize(file_get_contents($basis));
-  }
-  if (file_exists("$docroot/languages/$locale/helptext.txt")) {
-    $helpsrc = "$docroot/languages/$locale/helptext.txt";
-    $helptxt = "$docroot/languages/$locale/helptext.dot";
-  }
-  $jscript = "$docroot/webGui/javascript/translate.$locale.js";
-  if (!file_exists($jscript)) {
-    // create javascript file with translations
-    $source = [];
-    $files = glob("$docroot/languages/$locale/javascript*.txt",GLOB_NOSORT);
-    foreach ($files as $js) $source = array_merge($source,parse_lang_file($js));
-    if (count($source)) {
-      $script = ['function _(t){var l={};'];
-      foreach ($source as $key => $value) $script[] = "l[\"$key\"]=\"$value\";";
-      $script[] ="return l[t.replace(/\&amp;|[\?\{\}\|\&\~\!\[\]\(\)\/\\:\*^\.\"']|<.+?\/?>/g,'').replace(/  +/g,' ')]||t;}";
-      file_put_contents($jscript,implode('',$script));
-    } else {
-      file_put_contents($jscript,$return);
-    }
-  }
-  // split URI into translation levels
-  $uri = array_filter(explode('/',strtok($_SERVER['REQUEST_URI'],'?')));
-  foreach($uri as $more) {
-    $more = strtolower($more);
-    $text = "$docroot/languages/$locale/$more.txt";
-    if (file_exists($text)) {
-      // additional translations
-      $other = "$docroot/languages/$locale/$more.dot";
-      if (!file_exists($other)) file_put_contents($other,serialize(parse_lang_file($text)));
-      $language = array_merge($language,unserialize(file_get_contents($other)));
-    }
+if (file_exists($text)) {
+  $store = "$docroot/languages/$locale/translations.dot";
+  // global translations
+  if (!file_exists($store)) file_put_contents($store,serialize(parse_lang_file($text)));
+  $language = unserialize(file_get_contents($store));
+}
+if (file_exists($help)) {
+  // global help text
+  $store = "$docroot/languages/$locale/helptext.dot";
+  if (!file_exists($store)) file_put_contents($store,serialize(parse_lang_file($help)));
+  $language = array_merge($language,unserialize(file_get_contents($store)));
+}
+$jscript = "$docroot/webGui/javascript/translate.$locale.js";
+if (!file_exists($jscript)) {
+  // create javascript file with translations
+  $source = [];
+  $files = glob("$docroot/languages/$locale/javascript*.txt",GLOB_NOSORT);
+  foreach ($files as $js) $source = array_merge($source,parse_lang_file($js));
+  if (count($source)) {
+    $script = ['function _(t){var l={};'];
+    foreach ($source as $key => $value) $script[] = "l[\"$key\"]=\"$value\";";
+    $script[] ="return l[t.replace(/\&amp;|[\?\{\}\|\&\~\!\[\]\(\)\/\\:\*^\.\"']|<.+?\/?>/g,'').replace(/  +/g,' ')]||t;}";
+    file_put_contents($jscript,implode('',$script));
+  } else {
+    file_put_contents($jscript,$return);
   }
 }
-// add help text
-if (!file_exists($helptxt)) file_put_contents($helptxt,serialize(parse_lang_file($helpsrc)));
-$language = array_merge($language,unserialize(file_get_contents($helptxt)));
+// split URI into translation levels
+$uri = array_filter(explode('/',strtok($_SERVER['REQUEST_URI'],'?')));
+foreach($uri as $more) {
+  $more = strtolower($more);
+  $text = "$docroot/languages/$locale/$more.txt";
+  if (file_exists($text)) {
+    // additional translations
+    $other = "$docroot/languages/$locale/$more.dot";
+    if (!file_exists($other)) file_put_contents($other,serialize(parse_lang_file($text)));
+    $language = array_merge($language,unserialize(file_get_contents($other)));
+  }
+}
 ?>
