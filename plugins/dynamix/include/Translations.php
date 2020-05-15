bbc77fcaaffccee267062db4a7ea088148545ec3
bergware <bergware@users.noreply.github.com>
2020-05-15 09:17:38 +0200

    Multi-language support

--- plugins/dynamix/include/Translations.php
+++ plugins/dynamix/include/Translations.php
@@ -24,7 +24,7 @@ function parse_lang_file($file) {
   return array_filter(parse_ini_string(preg_replace(['/^(null|yes|no|true|false|on|off|none)=/mi','/^([^>].*)=(.*)$/m','/^:(.+_(help|plug)):$/m','/^:end$/m'],['$1.=','$1="$2"','_$1_="','"'],str_replace('"','&#34;',file_get_contents($file)))),'secured',ARRAY_FILTER_USE_BOTH);
 }
 function parse_help_file($file) {
-  return parse_ini_string(preg_replace(['/^([^:;].+)/m','/^:(.+_help):$/m','/^:end$/m'],['>$1','_$1_="','"'],str_replace('"','&#34;',file_get_contents($file))));
+  return parse_ini_string(preg_replace(['/^$/m','/^([^:;].+)$/m','/^:(.+_help):$/m','/^:end$/m'],['>','>$1','_$1_="','"'],str_replace('"','&#34;',file_get_contents($file))));
 }
 function parse_text($text) {
   return preg_replace_callback('/_\((.+?)\)_/m',function($m){return _($m[1]);},preg_replace(["/^:(.+_help):$/m","/^:(.+_plug):$/m","/^:end$/m"],["<?translate(\"_$1_\");?>","<?if (translate(\"_$1_\")):?>","<?endif;?>"],$text));
