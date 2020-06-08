6836bdd9c819cb68586029a847ec20468f647516
bergware <bergware@users.noreply.github.com>
2020-06-08 20:25:04 +0200

    Multi-language support

--- plugins/dynamix.plugin.manager/include/PluginHelpers.php
+++ plugins/dynamix.plugin.manager/include/PluginHelpers.php
@@ -13,6 +13,10 @@
 <?
 $docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
 
+if (!function_exists('_')) {
+  function _($text) {return $text;}
+}
+
 // Invoke the plugin command with indicated method
 function plugin($method, $arg = '') {
   global $docroot;
