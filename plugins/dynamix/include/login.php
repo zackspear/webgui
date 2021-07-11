e315336689c005a1e3ff3402fab1fc2f38299780
bergware <bergware@users.noreply.github.com>
2021-07-11 08:52:16 +0200

    Add translation support to login pages

--- plugins/dynamix/include/login.php
+++ plugins/dynamix/include/login.php
@@ -397,7 +397,7 @@ $theme_dark = in_array($display['theme'],['black','gray']);
                         cookieEnabled = document.cookie.indexOf("cookietest=")!=-1;
                         document.cookie = "cookietest=1; expires=Thu, 01-Jan-1970 00:00:01 GMT";
                         if (!cookieEnabled) {
-                            document.write("<p class='error'><?=_('Browser cookie support required for login')?></p>");
+                            document.write("<p class='error'><?=_('Browser cookie support required for Unraid OS webgui')?></p>");
                         }
                     </script>
                     <p>
