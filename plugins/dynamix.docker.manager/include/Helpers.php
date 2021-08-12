5aced8080abbcd2f1e105e042949aefbe86aa9fc
Jon Mills <40585882+TheFlyingBadger@users.noreply.github.com>
2021-08-12 14:18:30 +0800

    Add HOST_HOSTNAME and HOST_CONTAINERNAME env vars
    add extra environment variables to the container

--- plugins/dynamix.docker.manager/include/Helpers.php
+++ plugins/dynamix.docker.manager/include/Helpers.php
@@ -243,6 +243,11 @@ function xmlToCommand($xml, $create_paths=false) {
   $Variables[]   = 'TZ="'.$var['timeZone'].'"';
   // Add HOST_OS variable
   $Variables[]   = 'HOST_OS="Unraid"';
+  // Add HOST_HOSTNAME variable
+  $Variables[]   = 'HOST_HOSTNAME=$HOSTNAME';
+  // Add HOST_CONTAINERNAME variable
+  $Variables[]   = 'HOST_CONTAINERNAME='.escapeshellarg($xml['Name']);  
+  
   // Docker labels for WebUI and Icon
   $Labels[]      = 'net.unraid.docker.managed=dockerman';
   if (strlen($xml['WebUI']))  $Labels[] = 'net.unraid.docker.webui='.escapeshellarg($xml['WebUI']);
