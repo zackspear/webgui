3d9926d87bf241f30097f17c0deeed5fbd918622
Tom Mortensen <tomm@lime-technology.com>
2021-08-13 08:41:13 -0700

    Get HOSTNAME from $var['NAME']

--- plugins/dynamix.docker.manager/include/Helpers.php
+++ plugins/dynamix.docker.manager/include/Helpers.php
@@ -244,7 +244,7 @@ function xmlToCommand($xml, $create_paths=false) {
   // Add HOST_OS variable
   $Variables[]   = 'HOST_OS="Unraid"';
   // Add HOST_HOSTNAME variable 
-  $Variables[]   = 'HOST_HOSTNAME="'.gethostname().'"';
+  $Variables[]   = 'HOST_HOSTNAME="'.$var['NAME'].'"';
   // Add HOST_CONTAINERNAME variable
   $Variables[]   = 'HOST_CONTAINERNAME="'.$xml['Name'].'"';
   // Docker labels for WebUI and Icon
