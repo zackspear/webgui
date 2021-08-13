2f75a971fcf47113534ddbd45d21fdf43109b8e4
bergware <bergware@users.noreply.github.com>
2021-08-13 18:50:03 +0200

    Security update

--- plugins/dynamix/include/DiskList.php
+++ plugins/dynamix/include/DiskList.php
@@ -98,7 +98,7 @@ foreach ($disks as $name => $disk) {
     echo "<td>$type</td>";
     echo "<td>".my_scale(($disk['fsSize'])*1024, $unit)." $unit</td>";
     echo "<td>".my_scale($disk['fsFree']*1024, $unit)." $unit</td>";
-    echo "<td><a href=\"/$path/Browse?dir=/mnt/$name\"><i class='icon-u-tab' title=\""._('Browse')." /mnt/$name\"></i></a></td>";
+    echo "<td><a href=\"/$path/Browse?dir=/mnt/$name\"><i class=\"icon-u-tab\" title=\""._('Browse')." /mnt/$name\"></i></a></td>";
     echo "</tr>";
     foreach ($ssz2[$name] as $sharename => $sharesize) {
       if ($sharename=='share.total') continue;
