8cf7d5f9f357408edef497ae5d9a96284adbf89f
bergware <bergware@users.noreply.github.com>
2021-08-19 10:45:05 +0200

    Support multi-language in filetree display

--- plugins/dynamix/include/FileTree.php
+++ plugins/dynamix/include/FileTree.php
@@ -2,13 +2,14 @@
 /**
  * jQuery File Tree PHP Connector
  *
- * Version 1.1.1
+ * Version 1.2.0
  *
  * @author - Cory S.N. LaViska A Beautiful Site (http://abeautifulsite.net/)
  * @author - Dave Rogers - https://github.com/daverogers/jQueryFileTree
  *
  * History:
  *
+ * 1.2.0 - adapted by Bergware for use in Unraid - support UTF-8 encoding
  * 1.1.1 - SECURITY: forcing root to prevent users from determining system's file structure (per DaveBrad)
  * 1.1.0 - adding multiSelect (checkbox) support (08/22/2014)
  * 1.0.2 - fixes undefined 'dir' error - by itsyash (06/09/2014)
@@ -24,70 +25,38 @@
  * ex: $root = $_SERVER['DOCUMENT_ROOT'];
  */
 $root = '/';
-if( !$root ) exit("ERROR: Root filesystem directory not set in jqueryFileTree.php");
+if (!$root) exit("ERROR: Root filesystem directory not set in jqueryFileTree.php");
 
-$postDir = $root.(isset($_POST['dir']) ? $_POST['dir'] : '' );
-
-if (substr($postDir, -1) != '/') {
-	$postDir .= '/';
-}
-$postDir = preg_replace("#[\/]+#", "/", $postDir);
-
-$filters = (array)(isset($_POST['filter']) ? $_POST['filter'] : '');
-$match = (isset($_POST['match']) ? $_POST['match'] : '.*');
+$rootdir = preg_replace("#[\/]+#","/",$root.($_POST['dir'] ?? ''));
+$filters = (array)($_POST['filter'] ?? '');
+$match   = ($_POST['match'] ?? '.*');
 
 // set checkbox if multiSelect set to true
-$checkbox = ( isset($_POST['multiSelect']) && $_POST['multiSelect'] == 'true' ) ? "<input type='checkbox' />" : null;
-
-$returnDir	= $postDir;
+$checkbox = (isset($_POST['multiSelect']) && $_POST['multiSelect']=='true') ? "<input type='checkbox'>" : "";
 
 echo "<ul class='jqueryFileTree'>";
-
 // Parent dirs
-if ($_POST['show_parent'] == "true" ) {
-	echo "<li class='directory collapsed'>{$checkbox}<a href='#' rel='" . htmlspecialchars(dirname($postDir), ENT_QUOTES) . "/'>..</a></li>";
-}
+if ($_POST['show_parent']=='true') echo "<li class='directory collapsed'>$checkbox<a href='#' rel=\"".htmlspecialchars(dirname($rootdir),ENT_QUOTES)."/\">..</a></li>";
 
-if( file_exists($postDir) ) {
-
-	$files = scandir($postDir);
-
-	natcasesort($files);
-
-	if( count($files) > 2 ) { // The 2 accounts for . and ..
-
-		foreach( $files as $file ) {
-			if( file_exists($postDir . $file) && $file != '.' && $file != '..' ) {
-				if( is_dir($postDir . $file) ) {
-					$htmlRel	= htmlspecialchars($returnDir . $file, ENT_QUOTES);
-					$htmlName	= htmlspecialchars((strlen($file) > 33) ? substr($file,0,33).'...' : $file);
-
-					echo "<li class='directory collapsed'>{$checkbox}<a href='#' rel='" . $htmlRel . "/'>" . $htmlName . "</a></li>";
-				}
-			}
-		}
-
-		// All files
-		foreach( $files as $file ) {
-			if( file_exists($postDir . $file) && $file != '.' && $file != '..' ) {
-				if( !is_dir($postDir . $file) ) {
-					$htmlRel	= htmlspecialchars($returnDir . $file, ENT_QUOTES);
-					$htmlName	= htmlspecialchars($file);
-					$ext		= strtolower(preg_replace('/^.*\./', '', $file));
-
-    				foreach ($filters as $filter) {
-						if (empty($filter) | $ext==$filter) {
-							if (empty($match) || preg_match('/'.$match.'/', $file)) {
-								echo "<li class='file ext_{$ext}'>{$checkbox}<a href='#' rel='" . $htmlRel . "'>" . $htmlName . "</a></li>";
-							}
-						}
-					}
-				}
-			}
-		}
-
-	}
+if (file_exists($rootdir)) {
+  if (mb_substr($rootdir,-1)!='/') $rootdir .= '/';
+  $names = array_filter(scandir($rootdir),function($n){return $n!='.' && $n!='..';});
+  if (count($names)) {
+    natcasesort($names);
+    foreach ($names as $dir) if (is_dir($rootdir.$dir)) {
+      $htmlRel  = htmlspecialchars($rootdir.$dir,ENT_QUOTES);
+      $htmlName = htmlspecialchars(mb_strlen($dir)<=33 ? $dir : mb_substr($dir,0,33).'...');
+      echo "<li class='directory collapsed'>$checkbox<a href='#' rel=\"$htmlRel/\">$htmlName</a></li>";
+    }
+    foreach ($names as $file) if (is_file($rootdir.$file)) {
+      $htmlRel  = htmlspecialchars($rootdir.$file,ENT_QUOTES);
+      $htmlName = htmlspecialchars($file);
+      $ext      = mb_strtolower(pathinfo($file)['extension']);
+      foreach ($filters as $filter) if (empty($filter)||$ext==$filter) {
+        if (empty($match)||preg_match("#$match#",$file)) echo "<li class='file ext_$ext'>$checkbox<a href='#' rel=\"$htmlRel\">$htmlName</a></li>";
+      }
+    }
+  }
 }
 echo "</ul>";
-
 ?>
