5ea9245e05179bd98c65e347063e9822b2bfdee0
bergware <bergware@users.noreply.github.com>
2021-11-03 20:30:04 +0100

    Remove unused uptime update

--- plugins/dynamix/include/DefaultPageLayout.php
+++ plugins/dynamix/include/DefaultPageLayout.php
@@ -125,7 +125,6 @@ function updateTime() {
   var days = parseInt(uptime/86400);
   var hour = parseInt(uptime/3600%24);
   var mins = parseInt(uptime/60%60);
-  $('span.uptime').html(((days|hour|mins)?plus(days,"<?=_('day')?>","<?=_('days')?>",(hour|mins)==0)+plus(hour,"<?=_('hour')?>","<?=_('hours')?>",mins==0)+plus(mins,"<?=_('minute')?>","<?=_('minutes')?>",true):"<?=_('less than a minute')?>"));
   uptime += Math.round((now.getTime() - before.getTime())/1000);
   before = now;
   if (expiretime > 0) {
