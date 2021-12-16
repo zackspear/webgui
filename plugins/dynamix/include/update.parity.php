a7bde48b530f5ac804394698d0d466c71eafb820
bergware <bergware@users.noreply.github.com>
2021-12-16 03:59:08 +0100

    Added: Cumulative parity check

--- plugins/dynamix/include/update.parity.php
+++ plugins/dynamix/include/update.parity.php
@@ -30,36 +30,36 @@ if (isset($_POST['#apply'])) {
     $month = $_POST['month'] ?? '*';
     $day   = $_POST['day'] ?? '*';
     $write = $_POST['write'] ?? '';
-    $term  = $test = $end = '';
+    $term  = $test = $end1 = $end2 = '';
     switch ($dotm) {
       case '28-31':
         $term = '[[ $(date +%e -d +1day) -eq 1 ]] && ';
-        $end  = ' || :';
+        $end2 = ' || :';
         break;
       case 'W1':
         $dotm = '*';
         $term = '[[ $(date +%e) -le 7 ]] && ';
-        $end  = ' || :';
+        $end2 = ' || :';
         break;
       case 'W2':
         $dotm = '*';
         $term = '[[ $(date +%e -d -7days) -le 7 ]] && ';
-        $end  = ' || :';
+        $end2 = ' || :';
         break;
       case 'W3':
         $dotm = '*';
         $term = '[[ $(date +%e -d -14days) -le 7 ]] && ';
-        $end  = ' || :';
+        $end2 = ' || :';
         break;
       case 'W4':
         $dotm = '*';
         $term = '[[ $(date +%e -d -21days) -le 7 ]] && ';
-        $end  = ' || :';
+        $end2 = ' || :';
         break;
       case 'WL':
         $dotm = '*';
         $term = '[[ $(date +%e -d +7days) -le 7 ]] && ';
-        $end  = ' || :';
+        $end2 = ' || :';
         break;
     }
     $cron[] = "# Generated parity check schedule:";
@@ -68,12 +68,12 @@ if (isset($_POST['#apply'])) {
       $H = ($h + $_POST['duration']) % 24;
       if ($_POST['frequency']==7) {
         $test = '[[ $(date +%U -d @$(grep -Po "^sbSynced=\K\d+" /proc/mdstat) -ne $(date +%U) ]] && ';
-        $end  = ' || :';
+        $end1 = ' || :';
       }
       $cron[] = "$m $H * * * $ctrl pause &> /dev/null";
-      $cron[] = "$time * * $day {$test}{$ctrl} resume &> /dev/null$end";
+      $cron[] = "$time * * $day {$test}{$ctrl} resume &> /dev/null$end1";
     }
-    $cron[] = "$time $dotm $month $day {$term}{$mdcmd} check $write &> /dev/null$end";
+    $cron[] = "$time $dotm $month $day {$term}{$mdcmd} check $write &> /dev/null$end2";
   }
   parse_cron_cfg("dynamix", "parity-check", implode("\n",$cron)."\n");
   @unlink($memory);
