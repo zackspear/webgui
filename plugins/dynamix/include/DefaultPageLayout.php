ce67d0232766e13686d672e12733a5f42efe18c2
Tom Mortensen <tomm@lime-technology.com>
2022-03-09 11:28:43 -0800

    update footer copyright

--- plugins/dynamix/include/DefaultPageLayout.php
+++ plugins/dynamix/include/DefaultPageLayout.php
@@ -1,6 +1,6 @@
 <?PHP
-/* Copyright 2005-2021, Lime Technology
- * Copyright 2012-2021, Bergware International.
+/* Copyright 2005-2022, Lime Technology
+ * Copyright 2012-2022, Bergware International.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2,
@@ -557,7 +557,7 @@ default:
   echo "<span class='green strong'><i class='fa fa-play-circle'></i> "._('Array Started')."</span>$progress"; break;
 }
 echo "</span></span><span id='countdown'></span><span id='user-notice' class='red-text'></span>";
-echo "<span id='copyright'>Unraid&reg; webGui &copy;2021, Lime Technology, Inc.";
+echo "<span id='copyright'>Unraid&reg; webGui &copy;2022, Lime Technology, Inc.";
 echo " <a href='https://wiki.unraid.net/Manual' target='_blank' title=\""._('Online manual')."\"><i class='fa fa-book'></i> "._('manual')."</a>";
 echo "</span></div>";
 ?>
