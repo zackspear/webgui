634b6270b5ed7a1d721ab52d61dee7f0ab1a0edb
bergware <bergware@users.noreply.github.com>
2022-03-11 17:24:02 +0100

    Revert back to capitalization of device names

--- plugins/dynamix/include/PageBuilder.php
+++ plugins/dynamix/include/PageBuilder.php
@@ -71,7 +71,7 @@ function tab_title($title,$path,$tag) {
   $names = implode('|',array_merge(['disk','parity'],$pools));
   if (preg_match("/^($names)/",$title)) {
     $device = strtok($title,' ');
-    $title = str_replace($device,_(ucfirst(my_disk($device)),3),$title);
+    $title = str_replace($device,_(my_disk($device),3),$title);
   }
   $title = _(parse_text($title));
   if (!$tag || substr($tag,-4)=='.png') {
