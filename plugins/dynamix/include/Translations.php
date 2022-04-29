03212281aabe1f1728fd36cc6354dc95a1994e59
bergware <bergware@users.noreply.github.com>
2022-04-29 21:29:46 +0200

    Translations: fix creation of empty sessions

--- plugins/dynamix/include/Translations.php
+++ plugins/dynamix/include/Translations.php
@@ -1,6 +1,6 @@
 <?PHP
-/* Copyright 2005-2021, Lime Technology
- * Copyright 2012-2021, Bergware International.
+/* Copyright 2005-2022, Lime Technology
+ * Copyright 2012-2022, Bergware International.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2,
@@ -11,7 +11,7 @@
  */
 ?>
 <?
-if (session_status()==PHP_SESSION_NONE && !isset($login_locale)) {
+if (session_status()==PHP_SESSION_NONE && isset($_SESSION) && !isset($login_locale)) {
   session_start();
   session_write_close();
 }
