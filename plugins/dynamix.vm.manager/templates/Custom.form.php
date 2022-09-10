09454c2c0443906f77722f4416df8a95cb3a3280
SimonFair <39065407+SimonFair@users.noreply.github.com>
2022-09-10 19:15:07 +0100

    Fix boot order if 2+ disks added at create

--- plugins/dynamix.vm.manager/templates/Custom.form.php
+++ plugins/dynamix.vm.manager/templates/Custom.form.php
@@ -659,7 +659,7 @@
 					<?mk_dropdown_options($arrValidDiskBuses, $arrDisk['bus']);?>
 					</select>
 				_(Boot Order)_:
-				<input type="number" size="5" maxlength="5" id="wboot" class="narrow" style="width: 50px;" name="disk[<?=$i?>][boot]"   title="_(Boot order)_"  value="<?=$arrDisk['boot']?>" >
+				<input type="number" size="5" maxlength="5" id="disk[<?=$i?>][boot]" class="narrow" style="width: 50px;" name="disk[<?=$i?>][boot]"   title="_(Boot order)_"  value="<?=$arrDisk['boot']?>" >
 				</td>
 			</tr>
 		</table>
@@ -786,10 +786,9 @@
 					<select name="disk[{{INDEX}}][bus]" class="disk_bus narrow">
 					<?mk_dropdown_options($arrValidDiskBuses, '');?>
 					</select>
-				</td>
-
-				<td>_(Boot Order)_:</td><td>
-				<input type="text" size="5" maxlength="5" id="wboot" class="narrow" style="width: 50px;" name="disk[<?=$i?>][boot]"   title="_(Boot order)_"  value="<?=$arrdisk['boot']?>" disabled>
+				
+				_(Boot Order)_:
+				<input type="number" size="5" maxlength="5" id="disk[{{INDEX}}][boot]" class="narrow" style="width: 50px;" name="disk[{{INDEX}}][boot]"   title="_(Boot order)_"  value="" >
 				</td>
 			</tr>
 		</table>
