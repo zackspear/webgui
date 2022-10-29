594f711c74fcfc4da2519ac997e8f453b2d0891c
SimonFair <39065407+SimonFair@users.noreply.github.com>
2022-10-29 11:49:26 +0100

    Fix conflict.

--- plugins/dynamix.vm.manager/include/libvirt_helpers.php
+++ plugins/dynamix.vm.manager/include/libvirt_helpers.php
@@ -1190,6 +1190,7 @@
 		unset($old['cputune']['vcpupin'],$old['devices']['video'],$old['devices']['disk'],$old['devices']['interface'],$old['devices']['filesystem'],$old['cpu']['@attributes'],$old['os']['boot'],$old['os']['loader'],$old['os']['nvram']);
 		// Remove old CPU cache and features
 		unset($old['cpu']['cache'], $old['cpu']['feature']) ;
+		unset($old['features']['hyperv']) ;
 		// set namespace
 		$new['metadata']['vmtemplate']['@attributes']['xmlns'] = 'unraid';
 	}
