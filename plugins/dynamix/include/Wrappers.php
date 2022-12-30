2967e30bba1546cb64ad7cd1ec288725d1fca3bc
bergware <bergware@users.noreply.github.com>
2022-12-30 12:43:12 +0100

    PHP 8.1 support

--- plugins/dynamix/include/Wrappers.php
+++ plugins/dynamix/include/Wrappers.php
@@ -85,7 +85,7 @@ function ipaddr($ethX='eth0', $prot=4) {
 }
 // convert strftime to date format
 function my_date($fmt, $time) {
-  $legacy = ['%c' => 'D j M Y h:i:s A T','%A' => 'l','%Y' => 'Y','%B' => 'F','%e' => 'j','%d' => 'd','%m' => 'm','%I' => 'h','%H' => 'H','%M' => 'i','%S' => 's','%p' => 'a','%R' => 'H:i'];
+  $legacy = ['%c' => 'D j M Y h:i:s A T','%A' => 'l','%Y' => 'Y','%B' => 'F','%e' => 'j','%d' => 'd','%m' => 'm','%I' => 'h','%H' => 'H','%M' => 'i','%S' => 's','%p' => 'a','%R' => 'H:i', '%F' => 'Y-m-d'];
   return date(strtr($fmt,$legacy), $time);
 }
 ?>
