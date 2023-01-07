d6d879660ab692589121f6feaf0297f5888adef6
bergware <bergware@users.noreply.github.com>
2023-01-07 20:51:12 +0100

    Updated copyright 2023

--- plugins/dynamix.docker.manager/include/Helpers.php
+++ plugins/dynamix.docker.manager/include/Helpers.php
@@ -1,7 +1,7 @@
 <?PHP
-/* Copyright 2005-2022, Lime Technology
- * Copyright 2014-2022, Guilherme Jardim, Eric Schultz, Jon Panozzo.
- * Copyright 2012-2022, Bergware International.
+/* Copyright 2005-2023, Lime Technology
+ * Copyright 2014-2023, Guilherme Jardim, Eric Schultz, Jon Panozzo.
+ * Copyright 2012-2023, Bergware International.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2,
