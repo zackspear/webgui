d0a4d9c4c65f404cbcecd89cc3a2287f52702f5c
bergware <bergware@users.noreply.github.com>
2023-01-18 00:37:30 +0100

    PHP8 support

--- plugins/dynamix.docker.manager/include/DockerClient.php
+++ plugins/dynamix.docker.manager/include/DockerClient.php
@@ -17,7 +17,7 @@ libxml_use_internal_errors(true); # Suppress any warnings from xml errors.
 $docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
 
 // add translations
-if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI']!='docker' && substr($_SERVER['REQUEST_URI']??'',0,7) != '/Docker') {
+if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI']!='docker' && substr($_SERVER['REQUEST_URI'],0,7) != '/Docker') {
   $_SERVER['REQUEST_URI'] = 'docker';
   require_once "$docroot/webGui/include/Translations.php";
 }
