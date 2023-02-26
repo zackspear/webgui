65fb9bff4be0fb52e38589c8f1a9e0d43ce02c26
bergware <bergware@users.noreply.github.com>
2023-02-26 13:22:46 +0100

    DeviceInfo: fix flashing screen when zfs scrub is active

--- plugins/dynamix/include/FileSystemStatus.php
+++ plugins/dynamix/include/FileSystemStatus.php
@@ -17,12 +17,12 @@ $cmd  = $_POST['cmd'];
 $path = $_POST['path'];
 
 function btrfs($data) {return "btrfs-$data";}
-function zfs($data) {return "zfs-$data";}
+function zfs($data) {return "zfs-".strtok($data,' ');}
 
 switch ($cmd) {
 case 'status':
   exec("ps -C btrfs -o cmd=|awk '/$path\$/{print $2}'",$btrfs);
-  exec("/usr/sbin/zpool status|grep -om1 'scrub in progress'|grep -o scrub",$zfs);
+  exec("/usr/sbin/zpool status $path|grep -om1 'scrub in progress'",$zfs);
   echo implode(',',array_merge(array_map('btrfs',$btrfs),array_map('zfs',$zfs)));
   break;
 case 'btrfs-balance':
