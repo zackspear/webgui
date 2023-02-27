af2a838cd173cb43c83388ccb1553ba467cbec15
bergware <bergware@users.noreply.github.com>
2023-02-27 16:01:12 +0100

    PHP8 support

--- plugins/dynamix.docker.manager/include/Events.php
+++ plugins/dynamix.docker.manager/include/Events.php
@@ -1,7 +1,7 @@
 <?PHP
-/* Copyright 2005-2021, Lime Technology
- * Copyright 2014-2021, Guilherme Jardim, Eric Schultz, Jon Panozzo.
- * Copyright 2012-2021, Bergware International.
+/* Copyright 2005-2023, Lime Technology
+ * Copyright 2014-2023, Guilherme Jardim, Eric Schultz, Jon Panozzo.
+ * Copyright 2012-2023, Bergware International.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2,
@@ -13,57 +13,59 @@
 ?>
 <?
 $docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
+
 // add translations
 $_SERVER['REQUEST_URI'] = 'docker';
 require_once "$docroot/webGui/include/Translations.php";
 require_once "$docroot/webGui/include/Secure.php";
+require_once "$docroot/webGui/include/Wrappers.php";
 require_once "$docroot/plugins/dynamix.docker.manager/include/DockerClient.php";
 
 $DockerClient = new DockerClient();
-$action       = unscript($_REQUEST['action']??'');
-$container    = unbundle($_REQUEST['container']??'');
-$name         = unscript($_REQUEST['name']??'');
-$image        = unscript($_REQUEST['image']??'');
+$action       = unscript(_var($_REQUEST,'action'));
+$container    = unbundle(_var($_REQUEST,'container'));
+$name         = unscript(_var($_REQUEST,'name'));
+$image        = unscript(_var($_REQUEST,'image'));
 $arrResponse  = ['error' => _('Missing parameters')];
 
 switch ($action) {
-	case 'start':
-		if ($container) $arrResponse = ['success' => $DockerClient->startContainer($container)];
-		break;
-	case 'pause':
-		if ($container) $arrResponse = ['success' => $DockerClient->pauseContainer($container)];
-		break;
-	case 'stop':
-		if ($container) $arrResponse = ['success' => $DockerClient->stopContainer($container)];
-		break;
-	case 'resume':
-		if ($container) $arrResponse = ['success' => $DockerClient->resumeContainer($container)];
-		break;
-	case 'restart':
-		if ($container) $arrResponse = ['success' => $DockerClient->restartContainer($container)];
-		break;
-	case 'remove_container':
-		if ($container) $arrResponse = ['success' => $DockerClient->removeContainer($name, $container, 1)];
-		break;
-	case 'remove_image':
-		if ($image) $arrResponse = ['success' => $DockerClient->removeImage($image)];
-		break;
-	case 'remove_all':
-		if ($container && $image) {
-			// first: try to remove container
-			$ret = $DockerClient->removeContainer($name, $container, 2);
-			if ($ret === true) {
-				// next: try to remove image
-				$arrResponse = ['success' => $DockerClient->removeImage($image)];
-			} else {
-				// error: container failed to remove
-				$arrResponse = ['success' => $ret];
-			}
+case 'start':
+	if ($container) $arrResponse = ['success' => $DockerClient->startContainer($container)];
+	break;
+case 'pause':
+	if ($container) $arrResponse = ['success' => $DockerClient->pauseContainer($container)];
+	break;
+case 'stop':
+	if ($container) $arrResponse = ['success' => $DockerClient->stopContainer($container)];
+	break;
+case 'resume':
+	if ($container) $arrResponse = ['success' => $DockerClient->resumeContainer($container)];
+	break;
+case 'restart':
+	if ($container) $arrResponse = ['success' => $DockerClient->restartContainer($container)];
+	break;
+case 'remove_container':
+	if ($container) $arrResponse = ['success' => $DockerClient->removeContainer($name, $container, 1)];
+	break;
+case 'remove_image':
+	if ($image) $arrResponse = ['success' => $DockerClient->removeImage($image)];
+	break;
+case 'remove_all':
+	if ($container && $image) {
+		// first: try to remove container
+		$ret = $DockerClient->removeContainer($name, $container, 2);
+		if ($ret === true) {
+			// next: try to remove image
+			$arrResponse = ['success' => $DockerClient->removeImage($image)];
+		} else {
+			// error: container failed to remove
+			$arrResponse = ['success' => $ret];
 		}
-		break;
-	default:
-		$arrResponse = ['error' => _('Unknown action')." '$action'"];
-		break;
+	}
+	break;
+default:
+	$arrResponse = ['error' => _('Unknown action')." '$action'"];
+	break;
 }
 
 header('Content-Type: application/json');
