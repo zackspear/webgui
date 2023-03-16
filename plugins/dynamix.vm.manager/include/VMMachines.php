243fe697b0c70189967f8e31f1fac56d59389db2
bergware <bergware@users.noreply.github.com>
2023-03-16 05:14:06 +0100

    VM; add move indicator

--- plugins/dynamix.vm.manager/include/VMMachines.php
+++ plugins/dynamix.vm.manager/include/VMMachines.php
@@ -122,7 +122,7 @@ foreach ($vms as $vm) {
   }
 
   /* VM information */
-  echo "<tr parent-id='$i' class='sortable'><td class='vm-name' style='width:220px;padding:8px'>";
+  echo "<tr parent-id='$i' class='sortable'><td class='vm-name' style='width:220px;padding:8px'><i class='fa fa-arrows-v mover orange-text'></i>";
   echo "<span class='outer'><span id='vm-$uuid' $menu class='hand'>$image</span><span class='inner'><a href='#' onclick='return toggle_id(\"name-$i\")' title='click for more VM info'>$vm</a><br><i class='fa fa-$shape $status $color'></i><span class='state'>"._($status)."</span></span></span></td>";
   echo "<td>$desc</td>";
   echo "<td><a class='vcpu-$uuid' style='cursor:pointer'>$vcpu</a></td>";
