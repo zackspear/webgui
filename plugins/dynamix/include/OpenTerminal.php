af66d0d641532c27ccc5298de79e791718f968f8
bergware <bergware@users.noreply.github.com>
2023-03-24 16:34:47 +0100

    OpenTerminal: kill stalled instances

--- plugins/dynamix/include/OpenTerminal.php
+++ plugins/dynamix/include/OpenTerminal.php
@@ -74,6 +74,9 @@ case 'log':
   $name = unbundle($_GET['name']);
   $file = realpath($path.$_GET['more']);
   $sock = "/var/tmp/$name.sock";
+  exec("pgrep -f '$sock'", $pids);
+  // kill stalled instances
+  foreach ($pids as $pid) exec("kill $pid");
   exec("ttyd-exec -o -i '$sock' ".command($path,$file));
   break;
 case 'docker':
