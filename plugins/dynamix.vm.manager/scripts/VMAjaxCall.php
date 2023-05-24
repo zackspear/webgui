c7b88354db031f1c896f6f633f7936098a337db6
SimonFair <39065407+SimonFair@users.noreply.github.com>
2023-05-24 07:27:12 +0100

    Make Script executable

--- plugins/dynamix.vm.manager/scripts/VMAjaxCall.php
+++ plugins/dynamix.vm.manager/scripts/VMAjaxCall.php
@@ -12,31 +12,39 @@
 ?>
 <?
 $docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
+
 require_once "$docroot/webGui/include/Wrappers.php";
+
+// add translations
+$_SERVER['REQUEST_URI'] = '';
+$login_locale = _var($display,'locale');
+require_once "$docroot/plugins/dynamix.docker.manager/include/DockerClient.php";
 require_once "$docroot/webGui/include/Translations.php";
 require_once "$docroot/plugins/dynamix.vm.manager/include/libvirt_helpers.php";
 function write(...$messages){
-  $com = curl_init();
-  curl_setopt_array($com,[
-    CURLOPT_URL => 'http://localhost/pub/plugins?buffer_length=1',
-    CURLOPT_UNIX_SOCKET_PATH => '/var/run/nginx.socket',
-    CURLOPT_POST => 1,
-    CURLOPT_RETURNTRANSFER => true
-  ]);
-  foreach ($messages as $message) {
-    curl_setopt($com, CURLOPT_POSTFIELDS, $message);
-    curl_exec($com);
+	$com = curl_init();
+	curl_setopt_array($com,[
+	  CURLOPT_URL => 'http://localhost/pub/vmaction?buffer_length=1',
+	  CURLOPT_UNIX_SOCKET_PATH => '/var/run/nginx.socket',
+	  CURLOPT_POST => 1,
+	  CURLOPT_RETURNTRANSFER => true
+	]);
+	foreach ($messages as $message) {
+	  curl_setopt($com, CURLOPT_POSTFIELDS, $message);
+	  curl_exec($com);
+	}
+	curl_close($com);
   }
-  curl_close($com);
-}
 function execCommand_nchan($command) {
 	$waitID = mt_rand();
 	[$cmd,$args] = explode(' ',$command,2);
 	write("<p class='logLine'></p>","addLog\0<fieldset class='docker'><legend>"._('Command execution')."</legend>".basename($cmd).' '.str_replace(" -","<br>&nbsp;&nbsp;-",htmlspecialchars($args))."<br><span id='wait-$waitID'>"._('Please wait')." </span><p class='logLine'></p></fieldset>","show_Wait\0$waitID");
+	write("addToID\0 99\0 $action") ;
 	$proc = popen("$command 2>&1",'r');
 	while ($out = fgets($proc)) {
 	  $out = preg_replace("%[\t\n\x0B\f\r]+%", '',$out);
-	  write("addLog\0".htmlspecialchars($out));
+	  write("addToID\0 99\0".htmlspecialchars($out));
+	  sleep(5) ;
 	}
 	$retval = pclose($proc);
 	$out = $retval ? _('The command failed').'.' : _('The command finished successfully').'!';
@@ -46,31 +54,48 @@ function execCommand_nchan($command) {
 
 #{action:"snap-", uuid:uuid , snapshotname:target , remove:remove, free:free ,removemeta:removemeta ,keep:keep, desc:desc}
 $url = rawurldecode($argv[1]??'');
-$waitID = "bob" ;
+$waitID = mt_rand();
 $style = ["<style>"];
 $style[] = ".logLine{font-family:bitstream!important;font-size:1.2rem!important;margin:0;padding:0}";
 $style[] = "fieldset.docker{border:solid thin;margin-top:8px}";
 $style[] = "legend{font-size:1.1rem!important;font-weight:bold}";
 $style[] = "</style>";
-$path = "--current" ; $pivot = "yes" ; $action = " " ;
-write(implode($style)."<p class='logLine'></p>");
-write("<p class='logLine'></p>","addLog\0<fieldset class='docker'><legend>"._('Block Commit').": ".htmlspecialchars($path)."</legend><p class='logLine'></p><span id='wait-$waitID'>"._('Please wait')." </span></fieldset>","show_Wait\0$waitID");
-
+$path = "--current" ; $pivot = "yes" ; 
 foreach (explode('&', $url) as $chunk) {
     $param = explode("=", $chunk);
     if ($param) {
-#        write("addLog\0Parm" . sprintf("Value for parameter \"%s\" is \"%s\"<br/>\n", urldecode($param[0]), urldecode($param[1])));
 		${urldecode($param[0])} = urldecode($param[1]) ;
     }
 }
+$id = 1 ;
+write(implode($style)."<p class='logLine'></p>");
+write("addLog\0".htmlspecialchars("VMName $name "));
+write("addLog\0".htmlspecialchars("SNAP $snapshotname "));
+write("addLog\0".htmlspecialchars("Base $targetbase "));
+write("addLog\0".htmlspecialchars("Top $targettop "));
+	sleep(3) ;
+	write("stop_Wait\0$waitID") ;
+write("<p class='logLine'></p>","addLog\0<fieldset class='docker'><legend>"._("Block $action").": ".htmlspecialchars($path)."</legend><p class='logLine'></p><span id='wait-$waitID'>"._('Please wait')." </span></fieldset>","show_Wait\0$waitID");
 
-write("VMName $name ");
-write("SNAP $snapshotname ");
-write("Base $targetbase ");
-write("Top $targettop ");
-$path = "--current" ; $pivot = "yes" ; $action = " " ;
-vm_blockcommit($name,$snapshotname,$path,$targetbase,$targettop,$pivot,$action) ;
-#execCommand_nchan("ls /root") ;
+
+	sleep(3) ;
+	write("stop_Wait\0$waitID") ;
+$path = "--current" ; $pivot = "yes" ; 
+write("addToID\0 99\0 $action") ;
+switch ($action) {
+	case "commit":	
+	#	vm_blockcommit($name,$snapshotname,$path,$targetbase,$targettop,$pivot,' ') ;
+		break ;
+	case "copy":
+	#	vm_blockcopy($name,$snapshotname,$path,$targetbase,$targettop,$pivot,' ') ;
+		break;
+	case "pull":
+		vm_blockpull($name,$snapshotname,$path,$targetbase,$targettop,$pivot,' ') ;
+		break ;
+
+	}	
+#execCommand_nchan("ls /") ;
+write("stop_Wait\0$waitID") ;
 write('_DONE_','');
 
 ?>
