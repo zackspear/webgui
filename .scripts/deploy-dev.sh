ec80af7df7e214ac22bcd3d35082fcd647b65e6c
Zack Spear <hi@zackspear.com>
2023-06-29 12:26:20 -0700

    refactor: deploy-dev auto detect connect and change -host param

--- .scripts/deploy-dev.sh
+++ .scripts/deploy-dev.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# Path to store the last used server name
+# Path to store the last used server host
 state_file="$HOME/.webgui_deploy_state"
 
 # Function to display script usage and help information
 show_help() {
-  echo "Usage: $0 [SSH_SERVER_NAME] [-exclude-connect] [-exclude PATHS]"
+  echo "Usage: $0 [-host SSH_SERVER_HOST] [-exclude PATHS]"
   echo ""
   echo "Deploys the source directory to the specified SSH server using rsync."
   echo ""
-  echo "Positional Arguments:"
-  echo "  SSH_SERVER_NAME     The SSH server name to deploy to."
-  echo ""
   echo "Options:"
-  echo "  -exclude-connect    Exclude the directory 'emhttp/plugins/dynamix.my.servers'"
-  echo "  -exclude PATHS      Paths to exclude (comma-separated)"
+  echo "  -host SSH_SERVER_HOST    The SSH server host to deploy to."
+  echo "  -exclude PATHS           Paths to exclude (comma-separated)"
   echo ""
 }
 
@@ -24,15 +21,17 @@ if [[ $1 == "--help" || $1 == "-h" ]]; then
   exit 0
 fi
 
-# Parse command-line options
-exclude_connect="no"
+# Default values
+server_host=""
 exclude_paths=""
+
+# Parse command-line options
 while [[ $# -gt 0 ]]; do
   key="$1"
   case $key in
-    -exclude-connect)
-      exclude_connect="yes"
-      shift
+    -host)
+      server_host="$2"
+      shift 2
       ;;
     -exclude)
       exclude_paths="$2"
@@ -45,31 +44,37 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-# Read the last used server name from the state file
+# Read the last used server host from the state file
 if [[ -f "$state_file" ]]; then
-  last_server_name=$(cat "$state_file")
+  last_server_host=$(cat "$state_file")
 else
-  last_server_name=""
+  last_server_host=""
 fi
 
-# Read the server name from the command-line argument or use the last used server name as the default
-server_name="${1:-$last_server_name}"
+# Read the server host from the command-line option or use the last used server host as the default
+server_host="${server_host:-$last_server_host}"
 
-# Check if the server name is provided
-if [[ -z "$server_name" ]]; then
-  echo "Please provide the SSH server name."
+# Check if the server host is provided
+if [[ -z "$server_host" ]]; then
+  echo "Please provide the SSH server host using the -host option."
   echo "Use the --help option for more information."
   exit 1
 fi
 
-# Save the current server name to the state file
-echo "$server_name" > "$state_file"
+# Save the current server host to the state file
+echo "$server_host" > "$state_file"
 
 # Source directory path (current directory)
 source_directory="."
 
 # Destination directory path
 destination_directory="/usr/local"
+
+# Check if /usr/local/sbin/unraid-api exists on the remote server
+exclude_connect="no"
+if ssh "root@$server_host" "[ -f /usr/local/sbin/unraid-api ]"; then
+  exclude_connect="yes"
+fi
 
 # Exclude directory option
 exclude_option=""
@@ -86,7 +91,7 @@ if [[ -n "$exclude_paths" ]]; then
 fi
 
 # Rsync command
-rsync_command="rsync -amvz --relative --no-implied-dirs --progress --stats --exclude '/.*' --exclude '*/.*' $exclude_option \"$source_directory/\" \"root@${server_name}.local:$destination_directory/\""
+rsync_command="rsync -amvz --relative --no-implied-dirs --progress --stats --exclude '/.*' --exclude '*/.*' $exclude_option \"$source_directory/\" \"root@$server_host:$destination_directory/\""
 
 # Print the rsync command
 echo "Executing the following command:"
