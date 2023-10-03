ac0e67eeac33a49b835f3128daf3ec8ac0e9445d
bergware <bergware@users.noreply.github.com>
2023-10-03 10:21:19 +0200

    Updated logger registration

--- emhttp/plugins/dynamix/include/local_prepend.php
+++ emhttp/plugins/dynamix/include/local_prepend.php
@@ -10,14 +10,15 @@
  */
 ?>
 <?
-/* This file provides basic local setup for php cli and is
- * named in /etc/php/php.ini like this:
- * auto_prepend_file="/usr/local/emhttp/webGui/include/local_prepend.php"
- */
+// This file provides basic local setup for php cli and is
+// named in /etc/php/php.ini like this:
+// auto_prepend_file="/usr/local/emhttp/webGui/include/local_prepend.php"
+
 function csrf_terminate($reason) {
-    shell_exec("logger -- ".escapeshellarg("error: ".$_SERVER['REQUEST_URI']).": $reason csrf_token");
-    exit;
+  exec("logger -t webGUI -- \"error: {$_SERVER['REQUEST_URI']} - {$reason} csrf_token\"");
+  exit;
 }
+
 putenv('PATH=.:/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin');
 chdir('/usr/local/emhttp');
 setlocale(LC_ALL,'en_US.UTF-8');
@@ -28,14 +29,14 @@ ini_set("session.use_strict_mode", "1");
 // ini_set("session.cookie_samesite", $secure?'Strict':'Lax');
 ini_set("session.cookie_samesite", 'Lax');
 if (array_key_exists('HTTP_HOST', $_SERVER)) {
-    session_name("unraid_".md5(strstr($_SERVER['HTTP_HOST'].':', ':', true)));
+  session_name("unraid_".md5(strstr($_SERVER['HTTP_HOST'].':', ':', true)));
 }
 session_set_cookie_params(0, '/', null, $secure, true);
 if ($_SERVER['SCRIPT_NAME'] != '/login.php' && $_SERVER['SCRIPT_NAME'] != '/auth-request.php' && isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (!isset($var)) $var = parse_ini_file('state/var.ini');
-    if (!isset($var['csrf_token'])) csrf_terminate("uninitialized");
-    if (!isset($_POST['csrf_token'])) csrf_terminate("missing");
-    if ($var['csrf_token'] != $_POST['csrf_token']) csrf_terminate("wrong");
-    unset($_POST['csrf_token']);
+  if (!isset($var)) $var = parse_ini_file('state/var.ini');
+  if (!isset($var['csrf_token'])) csrf_terminate("uninitialized");
+  if (!isset($_POST['csrf_token'])) csrf_terminate("missing");
+  if ($var['csrf_token'] != $_POST['csrf_token']) csrf_terminate("wrong");
+  unset($_POST['csrf_token']);
 }
 ?>
