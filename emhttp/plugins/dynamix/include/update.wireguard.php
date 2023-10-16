1d031aa2628bfde607642bb1ffc0216d49b428d3
bergware <bergware@users.noreply.github.com>
2023-10-16 17:53:51 +0200

    wireguard update: add explanation about parsing

--- emhttp/plugins/dynamix/include/update.wireguard.php
+++ emhttp/plugins/dynamix/include/update.wireguard.php
@@ -229,6 +229,7 @@ function parseInput($vtun,&$input,&$x) {
   // assign values to parameters, be aware that certain parameters are assigned by parseInput itself
   // this is based on the sequence of processing
   global $conf,$user,$var,$default4,$default6,$vpn,$tunip;
+  $tunnel = $protocol = null; // satisfy code checkers
   $section = 0; $addPeer = false;
   foreach ($input as $key => $value) {
     if ($key[0]=='#') continue;
