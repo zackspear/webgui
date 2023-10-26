c3826a541685e82fb2e327b20f15badfec7f5f51
bergware <bergware@users.noreply.github.com>
2023-10-26 02:06:06 +0200

    Dashboard: update time when timezone is changed

---
+++ emhttp/plugins/dynamix/include/ResetTZ.php
@@ -0,0 +1,23 @@
+<?PHP
+/* Copyright 2005-2023, Lime Technology
+ * Copyright 2012-2023, Bergware International.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ */
+?>
+<?
+$docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
+$pid     = '/var/run/nchan.pid';
+$nchan   = 'webGui/nchan/update_3';
+
+if (is_file($pid) && exec("grep -Pom1 '^$nchan' $pid")) {
+  // restart update_3 script
+  exec("pkill -f $nchan");
+  exec("$docroot/$nchan &>/dev/null &");
+}
+?>
