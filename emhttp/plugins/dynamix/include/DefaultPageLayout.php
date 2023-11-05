247786bc5cfbe666477f00b667b65b4201e5deae
bergware <bergware@users.noreply.github.com>
2023-11-05 12:51:47 +0100

    Display settings: new setting "width"

--- emhttp/plugins/dynamix/include/DefaultPageLayout.php
+++ emhttp/plugins/dynamix/include/DefaultPageLayout.php
@@ -51,10 +51,10 @@ function annotate($text) {echo "\n<!--\n",str_repeat("#",strlen($text)),"\n$text
 <style>
 <?if (empty($display['width'])):?>
 @media (max-width:1280px){#displaybox{min-width:1280px;max-width:1280px;margin:0}}
-@media (min-width:1281px){#displaybox{min-width:1280px;margin:0 <?=$themes1?'10px':'auto'?>}}
+@media (min-width:1281px){#displaybox{min-width:1280px;max-width:1920px;margin:0 <?=$themes1?'10px':'auto'?>}}
 @media (min-width:1921px){#displaybox{min-width:1280px;max-width:1920px;margin:0 auto}}
 <?else:?>
-@media (max-width:1280px){#displaybox{min-width:1280px;max-width:1280px;margin:0}}
+@media (max-width:1280px){#displaybox{min-width:1280px;margin:0}}
 @media (min-width:1281px){#displaybox{min-width:1280px;margin:0 <?=$themes1?'10px':'auto'?>}}
 @media (min-width:1921px){#displaybox{min-width:1280px;margin:0 <?=$themes1?'20px':'auto'?>}}
 <?endif;?>
