96671aa9bea9f4406cf7d795e4d1b5f8d02969b2
Zack Spear <hi@zackspear.com>
2023-11-13 16:23:30 -0800

    chore: copyright comments

--- emhttp/plugins/dynamix.my.servers/include/myservers1.php
+++ emhttp/plugins/dynamix.my.servers/include/myservers1.php
@@ -1,3 +1,14 @@
+<?php
+/* Copyright 2005-2023, Lime Technology
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ */
+?>
 <style>
 #header {
     z-index: 102 !important;
