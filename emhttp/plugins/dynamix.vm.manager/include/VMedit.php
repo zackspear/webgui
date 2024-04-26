b0680ee1e1ea56d2cf625a57babbb3e19b8070dd
SimonFair <39065407+SimonFair@users.noreply.github.com>
2024-04-26 18:36:41 +0100

    Update VMedit.php

--- emhttp/plugins/dynamix.vm.manager/include/VMedit.php
+++ emhttp/plugins/dynamix.vm.manager/include/VMedit.php
@@ -96,13 +96,13 @@ if (isset($_GET['uuid'])) {
 $strSelectedTemplateUT = $strSelectedTemplate;
 if (strpos($strSelectedTemplate,"User-") !== false) $strSelectedTemplateUT = str_replace("User-","",$strSelectedTemplateUT);
 $newmodel = is_file("/etc/libvirt/qemu/newmodel");
-if ($newmodel && $arrLoad['form'] == "Custom.form.php") $arrLoad['form'] = "Custom.formXML.php"; 
+if ($newmodel && $arrLoad['form'] == "Custom.form.php") { $arrLoad['form'] = "Custom.formXML.php"; $inlineswt = '<input type="checkbox" class="inlineview">'; } else $inlineswt = ""
 ?>
 <link type="text/css" rel="stylesheet" href="<?autov('/plugins/dynamix.vm.manager/styles/dynamix.vm.manager.css')?>">
 <link type="text/css" rel="stylesheet" href="<?autov('/webGui/styles/jquery.filetree.css')?>">
 <link type="text/css" rel="stylesheet" href="<?autov('/webGui/styles/jquery.switchbutton.css')?>">
 
-<span class="status advancedview_panel" style="margin-top:<?=$top?>px;"><input type="checkbox" class="inlineview"><input type="checkbox" class="advancedview"></span>
+<span class="status advancedview_panel" style="margin-top:<?=$top?>px;"><?=$inlineswt?><input type="checkbox" class="advancedview"></span>
 <div class="domain">
 	<form id="vmform" method="POST">
 	<input type="hidden" name="domain[type]" value="kvm" />
