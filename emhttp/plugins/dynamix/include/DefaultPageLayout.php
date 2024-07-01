2a005f0c76e45ec7813857743e0c71f770c6b2d7
Zack Spear <hi@zackspear.com>
2024-07-01 16:44:11 -0700

    refactor: DockerManager default css with vars

--- emhttp/plugins/dynamix/include/DefaultPageLayout.php
+++ emhttp/plugins/dynamix/include/DefaultPageLayout.php
@@ -810,7 +810,9 @@ default:
 echo "</span></span><span id='countdown'></span><span id='user-notice' class='red-text'></span>";
 echo "<span id='copyright'>Unraid&reg; webGui &copy;2024, Lime Technology, Inc.";
 echo " <a href='https://docs.unraid.net/category/manual' target='_blank' title=\""._('Online manual')."\"><i class='fa fa-book'></i> "._('manual')."</a>";
-echo "</span></div>";
+echo "</span>";
+echo "<unraid-theme-switcher current='$theme'></unraid-theme-switcher>";
+echo "</div>";
 ?>
 <script>
 // Firefox specific workaround, not needed anymore in firefox version 100 and higher
