082d7d842bc94259e0a7b0e592d21f901abeea2c
SimonFair <39065407+SimonFair@users.noreply.github.com>
2024-10-06 11:26:34 +0100

    Revert "Add virtio gu(2d) as an option of template."
    This reverts commit 9a502776a1c323b4fc8b994c26292d1f6eb24da3.

--- emhttp/plugins/dynamix.vm.manager/include/libvirt_helpers.php
+++ emhttp/plugins/dynamix.vm.manager/include/libvirt_helpers.php
@@ -1115,8 +1115,7 @@ private static $encoding = 'UTF-8';
 		$arrValidVNCModels = [
 			'cirrus' => 'Cirrus',
 			'qxl' => 'QXL (best)',
-			'vmvga' => 'vmvga',
-			'virtio' => 'Virtio(2d)'
+			'vmvga' => 'vmvga'
 		];
 
 		return $arrValidVNCModels;
