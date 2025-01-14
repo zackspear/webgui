a6df9b388a5623c84f99a88d9df16df555355941
bergware <bergware@users.noreply.github.com>
2025-01-14 12:59:21 +0100

    Improved network information (including vlans)

--- emhttp/plugins/dynamix/include/NetworkInfo.php
+++ emhttp/plugins/dynamix/include/NetworkInfo.php
@@ -22,23 +22,29 @@ function port($eth) {
   return file_exists("$sys/br{$x}") ? "br${x}" : (file_exists("$sys/bond{$x}") ? "bond{$x}" : "eth{$x}");
 }
 exec("grep -Po 'nameserver \K.*' /etc/resolv.conf",$dns);
-$port   = port($_POST['port']);
+$eth    = $_POST['port'];
+$vlan   = $_POST['vlan'];
+$port   = port($eth).($vlan ? ".$vlan" : "");
 $v6on   = @file_get_contents("/proc/sys/net/ipv6/conf/$port/disable_ipv6")!=1;
-$error  = "<span class='red-text'>"._('missing')."</span>";
-$speed  = preg_replace('/^(\d+)/','$1 ',exec("ethtool $port | awk '$1==\"Speed:\" {print $2;exit}'")) ?: $error;
-$ipv4   = exec("ip -4 -br addr show scope global $port | awk '{\$1=\$2=\"\";print;exit}' | sed -r 's/ metric [0-9]+//g; s/\/[0-9]+//g' | xargs") ?: $error;
-$gw4    = exec("ip -4 route show default | awk '{print \$3;exit}'") ?: $error;
+$none   = _('None');
+$error  = "<span class='red-text'>"._('Missing')."</span>";
+$note   = $eth=='eth0' && !$vlan ? $error : $none;
+$link   = _(ucfirst(exec("ethtool $eth | awk '$1==\"Link\" {print $3;exit}'")))." (".exec("ethtool $eth | grep -Pom1 '^\s+Port: \K.*'").")";
+$speed  = _(preg_replace(['/^(\d+)/','/!/'],['$1 ',''],exec("ethtool $eth | awk '$1==\"Speed:\" {print $2;exit}'")));
+$ipv4   = exec("ip -4 -br addr show scope global $port | awk '{\$1=\$2=\"\";print;exit}' | sed -r 's/ metric [0-9]+//g; s/\/[0-9]+//g' | xargs") ?: $note;
+$gw4    = exec("ip -4 route show default dev $port | awk '{print \$3;exit}'") ?: $note;
 $dns4   = implode(' ',array_filter($dns,function($ns){return strpos($ns,':')===false;})) ?: $error;
 $domain = exec("grep -Pom1 'domain \K.*' /etc/resolv.conf") ?: '---';
 
 if ($v6on) {
-  $ipv6 = exec("ip -6 -br addr show scope global $port | awk '{\$1=\$2=\"\";print;exit}' | sed -r 's/ metric [0-9]+//g; s/\/[0-9]+//g' | xargs") ?: $error;
-  $gw6  = exec("ip -6 route show default | awk '{print \$3;exit}'") ?: $error;
+  $ipv6 = exec("ip -6 -br addr show scope global $port | awk '{\$1=\$2=\"\";print;exit}' | sed -r 's/ metric [0-9]+//g; s/\/[0-9]+//g' | xargs") ?: $note;
+  $gw6  = exec("ip -6 route show default dev $port | awk '{print \$3;exit}'") ?: $note;
   $dns6 = implode(' ',array_filter($dns,function($ns){return strpos($ns,':')!==false;})) ?: $error;
 }
 
 echo "<table style='text-align:left;font-size:1.2rem'>";
 echo "<tr><td>&nbsp;</td><td>&nbsp;</td></tr>";
+echo "<tr><td>"._('Link detected').":</td><td>$link</td></tr>";
 echo "<tr><td>"._('Interface speed').":</td><td>$speed</td></tr>";
 echo "<tr><td>"._('IPv4 address').":</td><td>$ipv4</td></tr>";
 echo "<tr><td>"._('IPv4 default gateway').":</td><td>$gw4</td></tr>";
