51a0a19572c63d56cdc2c7b2784bc0882a513975
bergware <bergware@users.noreply.github.com>
2025-01-14 13:50:55 +0100

    Update NetworkInfo.php

--- emhttp/plugins/dynamix/include/NetworkInfo.php
+++ emhttp/plugins/dynamix/include/NetworkInfo.php
@@ -21,7 +21,7 @@ function port($eth) {
   $x = preg_replace('/[^0-9]/','',$eth);
   return file_exists("$sys/br{$x}") ? "br${x}" : (file_exists("$sys/bond{$x}") ? "bond{$x}" : "eth{$x}");
 }
-exec("grep -Po 'nameserver \K.*' /etc/resolv.conf",$dns);
+exec("grep -Po 'nameserver \K.*' /etc/resolv.conf",$ns);
 $eth    = $_POST['port'];
 $vlan   = $_POST['vlan'];
 $port   = port($eth).($vlan ? ".$vlan" : "");
@@ -31,28 +31,44 @@ $error  = "<span class='red-text'>"._('Missing')."</span>";
 $note   = $eth=='eth0' && !$vlan ? $error : $none;
 $link   = _(ucfirst(exec("ethtool $eth | awk '$1==\"Link\" {print $3;exit}'")))." (".exec("ethtool $eth | grep -Pom1 '^\s+Port: \K.*'").")";
 $speed  = _(preg_replace(['/^(\d+)/','/!/'],['$1 ',''],exec("ethtool $eth | awk '$1==\"Speed:\" {print $2;exit}'")));
-$ipv4   = exec("ip -4 -br addr show scope global $port | awk '{\$1=\$2=\"\";print;exit}' | sed -r 's/ metric [0-9]+//g; s/\/[0-9]+//g' | xargs") ?: $note;
+$ipv4   = explode(' ',exec("ip -4 -br addr show scope global $port | awk '{\$1=\$2=\"\";print;exit}' | sed -r 's/ metric [0-9]+//g; s/\/[0-9]+//g' | xargs"));
 $gw4    = exec("ip -4 route show default dev $port | awk '{print \$3;exit}'") ?: $note;
-$dns4   = implode(' ',array_filter($dns,function($ns){return strpos($ns,':')===false;})) ?: $error;
+$dns4   = array_filter($ns,function($ns){return strpos($ns,':')===false;});
 $domain = exec("grep -Pom1 'domain \K.*' /etc/resolv.conf") ?: '---';
 
 if ($v6on) {
-  $ipv6 = exec("ip -6 -br addr show scope global $port | awk '{\$1=\$2=\"\";print;exit}' | sed -r 's/ metric [0-9]+//g; s/\/[0-9]+//g' | xargs") ?: $note;
+  $ipv6 = explode(' ',exec("ip -6 -br addr show scope global $port | awk '{\$1=\$2=\"\";print;exit}' | sed -r 's/ metric [0-9]+//g; s/\/[0-9]+//g' | xargs"));
   $gw6  = exec("ip -6 route show default dev $port | awk '{print \$3;exit}'") ?: $note;
-  $dns6 = implode(' ',array_filter($dns,function($ns){return strpos($ns,':')!==false;})) ?: $error;
+  $dns6 = array_filter($ns,function($ns){return strpos($ns,':')!==false;});
 }
 
 echo "<table style='text-align:left;font-size:1.2rem'>";
 echo "<tr><td>&nbsp;</td><td>&nbsp;</td></tr>";
 echo "<tr><td>"._('Interface link').":</td><td>$link</td></tr>";
 echo "<tr><td>"._('Interface speed').":</td><td>$speed</td></tr>";
-echo "<tr><td>"._('IPv4 address').":</td><td>$ipv4</td></tr>";
+if (count($ipv4)) foreach ($ipv4 as $ip) {
+  echo "<tr><td>"._('IPv4 address').":</td><td>$ip</td></tr>";
+} else {
+  echo "<tr><td>"._('IPv4 address').":</td><td>$note</td></tr>";
+}
 echo "<tr><td>"._('IPv4 default gateway').":</td><td>$gw4</td></tr>";
-echo "<tr><td>"._('IPv4 DNS servers').":</td><td>$dns4</td></tr>";
+if (count($dns4)) foreach ($dns4 as $dns) {
+  echo "<tr><td>"._('IPv4 DNS server').":</td><td>$dns</td></tr>";
+} else {
+  echo "<tr><td>"._('IPv4 DNS server').":</td><td>$error</td></tr>";
+}
 if ($v6on) {
-  echo "<tr><td>"._('IPv6 address').":</td><td>$ipv6</td></tr>";
+  if (count($ipv6)) foreach ($ipv6 as $ip) {
+    echo "<tr><td>"._('IPv6 address').":</td><td>$ip</td></tr>";
+  } else {
+    echo "<tr><td>"._('IPv6 address').":</td><td>$note</td></tr>";
+  }
   echo "<tr><td>"._('IPv6 default gateway').":</td><td>$gw6</td></tr>";
-  echo "<tr><td>"._('IPv6 DNS servers').":</td><td>$dns6</td></tr>";
+  if (count($dns6)) foreach ($dns6 as $dns) {
+    echo "<tr><td>"._('IPv6 DNS server').":</td><td>$dns</td></tr>";
+  } else {
+    echo "<tr><td>"._('IPv6 DNS server').":</td><td>$error</td></tr>";
+  }
 }
 echo "<tr><td>"._('Domain name').":</td><td>$domain</td></tr>";
 echo "</table>";
