844f86a10f548b813ec214a8e8db6ae98ca9f5e5
bergware <bergware@users.noreply.github.com>
2025-01-18 15:03:10 +0100

    Add "Move To End" icon

--- emhttp/plugins/dynamix/include/DefaultPageLayout.php
+++ emhttp/plugins/dynamix/include/DefaultPageLayout.php
@@ -1135,7 +1135,7 @@ $(function() {
     });
   }
   $('form').append($('<input>').attr({type:'hidden', name:'csrf_token', value:csrf_token}));
-  setInterval(function(){if ($(document).height() > $(window).height()) $('.move_to_end').fadeIn(scrollDuration);},250);
+  setInterval(function(){if ($(document).height() > $(window).height()) $('.move_to_end').fadeIn(scrollDuration); else $('.move_to_end').fadeOut(scrollDuration);},250);
 });
 
 var gui_pages_available = [];
