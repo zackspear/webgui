26be6290f2d7b2212dbc53d1010dab078c017d05
bergware <bergware@users.noreply.github.com>
2025-02-13 22:19:21 +0100

    VM: textual update

--- emhttp/plugins/dynamix.vm.manager/templates/Custom.form.php
+++ emhttp/plugins/dynamix.vm.manager/templates/Custom.form.php
@@ -350,8 +350,9 @@ if ($snapshots != null && count($snapshots) && !$boolNew) {
 	<tr id="zfs-name" class="hidden">
 		<td></td>
 		<td>
-			<span id="snap-rename" class="orange-text"><i class="fa fa-warning"></i> _(Rename disabled, <?=$snapcount?> snapshot(s) exists)_.</span>
-			<span class="orange-text"><i class="fa fa-warning"></i> _(Name contains invalid characters or does not start with an alphanumberic for a ZFS storage location<br>Only these special characters are valid Underscore (_) Hyphen (-) Colon (:) Period (.))_</span>
+			<span class="orange-text"><i class="fa fa-fw fa-warning"></i> _(Rename disabled, <?=$snapcount?> snapshot(s) exists)_.</span><br>
+			<span class="orange-text"><i class="fa fa-fw fa-warning"></i> _(Name contains invalid characters or does not start with an alphanumberic for a ZFS storage location)_</span><br>
+			<span class="orange-text"><i class="fa fa-fw fa-info-circle"></i> _(Only these special characters are valid Underscore (_) Hyphen (-) Colon (:) Period (.))_</span>
 		</td>
 		<td></td>
 	</tr>
