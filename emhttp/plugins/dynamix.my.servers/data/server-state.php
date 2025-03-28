8596a2dbeafa683977bc0acacf2516a02a3816fc
Zack Spear <hi@zackspear.com>
2025-03-28 14:39:15 -0700

    fix: ensure server-state.php loads necessary variables

--- emhttp/plugins/dynamix.my.servers/data/server-state.php
+++ emhttp/plugins/dynamix.my.servers/data/server-state.php
@@ -1,5 +1,6 @@
 <?php
 $docroot ??= ($_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp');
+$var = (array)parse_ini_file('state/var.ini'); // required for state.php - don't remove unless you've refactored the code to work without it
 require_once "$docroot/plugins/dynamix.my.servers/include/state.php";
 
 $serverState = new ServerState();
