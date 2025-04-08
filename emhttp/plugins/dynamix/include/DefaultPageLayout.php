7d763f3f312df90408d88e81d3f1c6fee93a0e0f
Zack Spear <hi@zackspear.com>
2025-04-07 18:02:31 -0700

    refactor: consolidate nchan script creation in DefaultPageLayout.php for found pages

--- emhttp/plugins/dynamix/include/DefaultPageLayout.php
+++ emhttp/plugins/dynamix/include/DefaultPageLayout.php
@@ -38,6 +38,20 @@ if (!file_exists($notes)) file_put_contents($notes,shell_exec("$docroot/plugins/
 
 $taskPages = find_pages('Tasks');
 $buttonPages = find_pages('Buttons');
+$pages = []; // finds subpages
+if (!empty($myPage['text'])) $pages[$myPage['name']] = $myPage;
+if (_var($myPage,'Type')=='xmenu') $pages = array_merge($pages, find_pages($myPage['name']));
+
+// build nchan scripts from found pages
+foreach ($taskPages as $button) {
+  if (isset($button['Nchan'])) nchan_merge($button['root'], $button['Nchan']);
+}
+foreach ($buttonPages as $button) {
+  if (isset($button['Nchan'])) nchan_merge($button['root'], $button['Nchan']);
+}
+foreach ($pages as $page) {
+  if (isset($page['Nchan'])) nchan_merge($page['root'], $page['Nchan']);
+}
 
 function annotate($text) {echo "\n<!--\n",str_repeat("#",strlen($text)),"\n$text\n",str_repeat("#",strlen($text)),"\n-->\n";}
 
@@ -146,8 +160,6 @@ foreach ($taskPages as $button) {
   $play = $task==$page ? " active" : "";
   echo "<div class='nav-item{$play}'>";
   echo "<a href=\"/$page\" onclick=\"initab('/$page')\">"._(_var($button,'Name',$page))."</a></div>";
-  // create list of nchan scripts to be started
-  if (isset($button['Nchan'])) nchan_merge($button['root'], $button['Nchan']);
 }
 unset($taskPages);
 echo "</div>";
@@ -174,8 +186,6 @@ foreach ($buttonPages as $button) {
   } else {
     echo "<div class='{$button['Link']}'></div>";
   }
-  // create list of nchan scripts to be started
-  if (isset($button['Nchan'])) nchan_merge($button['root'], $button['Nchan']);
 }
 
 echo "<div class='nav-user show'><a id='board' href='#' class='hand'><b id='bell' class='icon-u-bell system'></b></a></div>";
@@ -188,9 +198,6 @@ unset($buttonPages,$button);
 // Build page content
 echo "<div class='tabs'>";
 $tab = 1;
-$pages = [];
-if (!empty($myPage['text'])) $pages[$myPage['name']] = $myPage;
-if (_var($myPage,'Type')=='xmenu') $pages = array_merge($pages, find_pages($myPage['name']));
 if (isset($myPage['Tabs'])) $display['tabs'] = strtolower($myPage['Tabs'])=='true' ? 0 : 1;
 $tabbed = $display['tabs']==0 && count($pages)>1;
 
@@ -234,8 +241,6 @@ foreach ($pages as $page) {
       echo "<div class=\"Panel\"><a href=\"/$path/{$pg['name']}\" onclick=\"$.cookie('one','tab1')\"><span>$icon</span><div class=\"PanelText\">"._($title)."</div></a></div>";
     }
   }
-  // create list of nchan scripts to be started
-  if (isset($page['Nchan'])) nchan_merge($page['root'], $page['Nchan']);
   annotate($page['file']);
   // include page specific stylesheets (if existing)
   $css = "/{$page['root']}/sheets/{$page['name']}";
