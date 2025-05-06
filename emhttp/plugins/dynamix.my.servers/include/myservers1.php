2401c2b8d360c7920bd7a720abc2c01d2b260400
Zack Spear <hi@zackspear.com>
2025-05-06 15:41:20 -0700

    chore: remove unused CSS styles from myservers1.php
    - Deleted obsolete styles related to the header and Downgrade icon rotation.
    - Cleaned up the file to improve maintainability and reduce clutter.

--- emhttp/plugins/dynamix.my.servers/include/myservers1.php
+++ emhttp/plugins/dynamix.my.servers/include/myservers1.php
@@ -9,40 +9,6 @@
  * all copies or substantial portions of the Software.
  */
 ?>
-<style>
-#header {
-    z-index: 102 !important;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-pack: justify;
-    -ms-flex-pack: justify;
-    justify-content: space-between;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-}
-#header unraid-i18n-host {
-    font-size: 16px;
-    margin-left: auto;
-    height: 100%;
-}
-
-/**
- * Tools page, rotate the Downgrade icon to prevent needing to add a new icon to the icon font.
- * The pseudo element is targeted here otherwise the rotation of the span would mess up spacing with the text.
- */
-a[href="/Tools/Downgrade"] .icon-update:before {
-    display: inline-block; /* required otherwise the rotation won't work */
-    rotate: 180deg;
-}
-/* overriding #header .logo svg */
-#header .logo .partner-logo svg {
-    fill: var(--header-text-primary);
-    width: auto;
-    height: 28px;
-}
-</style>
 <?php
 require_once("$docroot/plugins/dynamix.my.servers/include/web-components-extractor.php");
 
