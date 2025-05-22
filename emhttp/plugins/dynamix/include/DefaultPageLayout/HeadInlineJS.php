b8cb46cb287b9407157e610c45e5133efe3c2304
Zack Spear <hi@zackspear.com>
2025-05-22 16:59:33 -0700

    feat: remove NotificationsArchive page and related files
    - Deleted the NotificationsArchive.page, NotificationsArchive.php, and associated CSS file as they are no longer needed.
    - This change helps streamline the codebase and remove obsolete components.
    - Notifications replaced via unraid-api inclusion

--- emhttp/plugins/dynamix/include/DefaultPageLayout/HeadInlineJS.php
+++ emhttp/plugins/dynamix/include/DefaultPageLayout/HeadInlineJS.php
@@ -477,10 +477,6 @@ function digits(number) {
   return 'three';
 }
 
-function viewHistory() {
-  location.replace('/Tools/NotificationsArchive');
-}
-
 function flashReport() {
   $.post('/webGui/include/Report.php',{cmd:'config'},function(check){
     if (check>0) addBannerWarning("<?=_('Your flash drive is corrupted or offline').'. '._('Post your diagnostics in the forum for help').'.'?> <a target='_blank' href='https://docs.unraid.net/go/changing-the-flash-device/'><?=_('See also here')?></a>");
