f13fdd3f13e38f690a6ec5e03c2147e0ffbfc2b1
Squidly271 <unraidsquid@gmail.com>
2025-08-06 11:32:01 -0400

    Update CreateDocker.php

--- emhttp/plugins/dynamix.docker.manager/include/CreateDocker.php
+++ emhttp/plugins/dynamix.docker.manager/include/CreateDocker.php
@@ -1186,7 +1186,7 @@ _(Use Tailscale)_:
 <div markdown="1" class="TSdivider noshow">
 <?if($TS_expiry_diff->invert):?>
 <b>_(Warning)_</b>:
-: <span></span><b>Tailscale Key expired!</b> <a href="<?=$TS_MachinesLink?>" target='_blank'>Renew/Disable key expiry</a> for '<b><?=$TS_HostNameActual?></b>'.</span>
+: <span><b>Tailscale Key expired!</b> <a href="<?=$TS_MachinesLink?>" target='_blank'>Renew/Disable key expiry</a> for '<b><?=$TS_HostNameActual?></b>'.</span>
 <?else:?>
 <b>_(Warning)_</b>:
 : <span>Tailscale Key will expire in <b><?=$TS_expiry_diff->days?> days</b>! <a href="<?=$TS_MachinesLink?>" target='_blank'>Disable Key Expiry</a> for '<b><?=$TS_HostNameActual?></b>'.</span>
