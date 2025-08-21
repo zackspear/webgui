ac906029be1d6a8a7f7529ccd5ef9ae8ef624306
Zack Spear <hi@zackspear.com>
2025-08-21 10:44:57 -0700

    fix: update favicon link structure for browser consistency

--- emhttp/plugins/dynamix/include/DefaultPageLayout/Favicon.php
+++ emhttp/plugins/dynamix/include/DefaultPageLayout/Favicon.php
@@ -1,5 +1,4 @@
-<link type="image/png" rel="shortcut icon" href="/webGui/images/<?= _var($var, 'mdColor', 'red-on') ?>.png">
-<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link type="image/png" rel="icon" href="/webGui/images/<?= _var($var, 'mdColor', 'red-on') ?>.png">
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 <meta name="apple-mobile-web-app-title" content="<?= _var($var, 'NAME') ?>" />
 <link rel="manifest" href="/manifest.json" />
