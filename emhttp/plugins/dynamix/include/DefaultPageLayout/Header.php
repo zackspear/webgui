40b62796df59080ba29192b6482c5df81297523a
Squidly271 <unraidsquid@gmail.com>
2025-11-12 17:42:18 -0500

    Fix: Array usage bar sidenav theme

--- emhttp/plugins/dynamix/include/DefaultPageLayout/Header.php
+++ emhttp/plugins/dynamix/include/DefaultPageLayout/Header.php
@@ -1,5 +1,7 @@
 <div id="header" class="<?=$display['banner']?>">
     <unraid-header-os-version></unraid-header-os-version>
-
+    <? if ($display['usage'] && $themeHelper->isSidebarTheme()): ?>
+        <span id='array-usage-sidenav'></span>
+    <? endif; ?>
     <?include "$docroot/plugins/dynamix.my.servers/include/myservers2.php"?>
 </div>
